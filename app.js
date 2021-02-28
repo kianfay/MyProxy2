6ac1a71c4f7500f83768e269975e4f02820e3c9d
Kian Fay <kifay@tcd.ie>
2021-02-28 23:17:40 +0000

    not working

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ var url = require('url');
 // Create proxy server and start listening for requests
 const proxyServer = net.createServer();
 
+// For debugging reasons
+proxyServer.maxConnections = 1;
+
 proxyServer.on('connection', (socket) => {
+    
+    /* proxyServer.close(() =>{
+        console.log("closing server for debugging reasons")
+    }) */
 
 /*  Set encoding so we can deal with incloming HTTP requests as strings, 
 **  even if just for debugging */
     socket.setEncoding('utf8');
+
+    var ProxyCLientS = null;
 
 /*  Define a function which abstracts the forwarding of a request to the webserver, and
 **  also the forwarding of the response */
@@ -25,32 +34,83 @@ proxyServer.on('connection', (socket) => {
                 return;
             }
             
-            console.log("Parsed URL translated to IP"  + fam + ": " + addr);
+            console.log("Parsed URL ( " + host + " ) translated to IP"  + fam + ": " + addr);
 
             // create a TCP connection to the webserver's IP (using 80 for any HTTP requests)
             ProxyCLient = net.connect(port ,addr, () => {
                 console.log("connected to webserver");
             })
 
-            //ProxyCLient.setEncoding('utf-8');
+            ProxyCLient.setEncoding('utf-8');
             
             ProxyCLient.write(forwardData);
               
             ProxyCLient.on('data', (dataFromWebServer) => {
+                console.log("Recieved this data as a response: " + dataFromWebServer);
+                console.log("forwarding this RESPONSE to whichever source requested " + host);
                 socket.write(dataFromWebServer)
             }) 
         });
     }
 
+    function forwardRequestSSL(host, port, forwardData){
+        
+        console.log("Establishing TCP connection with: " + host + " at port " + port);
+        
+        // use DNS to translate hostname to IP
+        dns.lookup(host, (error, addr, fam) => {
+                
+            if(error){
+                console.log("Error parsing this hostname: " + error);
+                return;
+            }
+            
+            console.log("Parsed URL ( " + host + " ) translated to IP"  + fam + ": " + addr);
+
+            // create a TCP connection to the webserver's IP (using 80 for any HTTP requests)
+            ProxyCLientS = net.connect(port ,addr, () => {
+                console.log("connected to webserver");
+
+                socket.write("HTTP/1.1 200 Connection established\r\n\r\n");
+
+                /* socket.on('data', (data) => {
+                    console.log("Tunelling data from client to server")
+                    ProxyCLientS.write(data);
+                }) */
+            })
+
+            ProxyCLientS.on('error', (error) => {
+                console.log('Received an error at the connection between client and proxy')
+                console.log(error)
+            });
+
+            ProxyCLientS.on('data', (dataFromWebServer) => {
+                console.log("Tunelling data from server to client");
+                console.log("forwarding this RESPONSE to whichever source requested " + host);
+                socket.write(dataFromWebServer)
+            });
+        });
+    }
+
+    socket.on('error', (error) => {
+        console.log('Received an error at the connection between client and proxy')
+    })
 
     // Set callback for when a request is received to forward the data
     socket.on('data', (data) => {
+
+        if(ProxyCLientS != null){
+            console.log("trying to create tunnel")
+            if(ProxyCLientS.write(data)){
+                console.log("tunnel created. data sent: "+ data.slice(1,10))
+            }
+        }
 
         //** Debugging **//
         //console.log("Verb: " + verb);
         //console.log("Destination: " + destination);
         console.log('\n');
-        console.log(data);
+        //console.log(data);
 
         // Isolate the HTTP verb at [0]
         var httpSplit = data.split(' ');
@@ -71,7 +131,8 @@ proxyServer.on('connection', (socket) => {
             match = /Host: (.+):.+\r\n.*/.exec(data);
             if(match){
                 console.log("Host is: " + match[1]);
-                forwardRequest(match[1], 443, data);
+                socket.setKeepAlive(true)
+                forwardRequestSSL(match[1], 443, data);
             }
         }
         
