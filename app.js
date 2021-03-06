38d62b9261bb42a37d80fadc26839225ad356461
Kian Fay <kifay@tcd.ie>
2021-03-06 21:46:07 +0000

    Working HTTP(S) proxy

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ var url = require('url');
 // Create proxy server and start listening for requests
 const proxyServer = net.createServer();
 
+// For debugging reasons
+//proxyServer.maxConnections = 1;
+
 proxyServer.on('connection', (socket) => {
 
 /*  Set encoding so we can deal with incloming HTTP requests as strings, 
 **  even if just for debugging */
-    socket.setEncoding('utf8');
+    //socket.setEncoding('utf8');
+
+    var ProxyCLientS = null;
 
 /*  Define a function which abstracts the forwarding of a request to the webserver, and
 **  also the forwarding of the response */
@@ -25,7 +30,7 @@ proxyServer.on('connection', (socket) => {
                 return;
             }
             
-            console.log("Parsed URL translated to IP"  + fam + ": " + addr);
+            console.log("Parsed URL ( " + host + " ) translated to IP"  + fam + ": " + addr);
 
             // create a TCP connection to the webserver's IP (using 80 for any HTTP requests)
             ProxyCLient = net.connect(port ,addr, () => {
@@ -37,23 +42,69 @@ proxyServer.on('connection', (socket) => {
             ProxyCLient.write(forwardData);
               
             ProxyCLient.on('data', (dataFromWebServer) => {
+                console.log("Recieved this data as a response: " + dataFromWebServer);
+                console.log("forwarding this RESPONSE to whichever source requested " + host);
                 socket.write(dataFromWebServer)
             }) 
         });
     }
 
+    function forwardRequestSSL(host, port, forwardData){
+        
+        console.log("Establishing TCP connection with: " + host + " at port " + port);
+        
+        // use DNS to translate hostname to IP
+        dns.lookup(host, (error, addr, fam) => {
+                
+            if(error){
+                console.log("Error parsing this hostname: " + error);
+                return;
+            }
+            
+            console.log("Parsed URL ( " + host + " ) translated to IP"  + fam + ": " + addr);
+
+            // create a TCP connection to the webserver's IP (using 80 for any HTTP requests)
+            ProxyCLientS = net.connect(port ,addr, () => {
+                console.log("connected to webserver");
+
+                ProxyCLientS.setKeepAlive(true)
+
+                ProxyCLientS.on('error', (error) => {
+                    console.log('Received an error at the connection between server and proxy')
+                    console.log(error)
+                });
+
+                ProxyCLientS.on('data', (dataFromWebServer) => {
+                    console.log("Tunelling data from server to client");
+                    console.log("forwarding this RESPONSE to whichever source requested " + host);
+                    socket.write(dataFromWebServer)
+                });
+
+                socket.write("HTTP/1.1 200 Connection established\r\n\r\n");
+
+            })
+        });
+    }
+
+    socket.on('error', (error) => {
+        console.log('Received an error at the connection between client and proxy')
+    })
 
     // Set callback for when a request is received to forward the data
     socket.on('data', (data) => {
 
-        //** Debugging **//
-        //console.log("Verb: " + verb);
-        //console.log("Destination: " + destination);
+        if(ProxyCLientS != null){
+            console.log("trying to create tunnel")
+            if(ProxyCLientS.write(data)){
+                console.log("tunnel created. data sent: "+ data.slice(1,10))
+            }
+        }
+
         console.log('\n');
         console.log(data);
 
         // Isolate the HTTP verb at [0]
-        var httpSplit = data.split(' ');
+        var httpSplit = data.toString().split(' ');
         var verb = httpSplit[0];
         //console.log(verb);
 
@@ -71,55 +122,12 @@ proxyServer.on('connection', (socket) => {
             match = /Host: (.+):.+\r\n.*/.exec(data);
             if(match){
                 console.log("Host is: " + match[1]);
-                forwardRequest(match[1], 443, data);
+                socket.setKeepAlive(true)
+                forwardRequestSSL(match[1], 443, data);
             }
         }
-        
-        console.log('\n');
-
-/* 
-        // extract the host (note, in http://example.org, the example.org is the host. If we 
-        // pass the sntire destination to dns.lookup it won't work, we must pass the host)
-        const destURL = new URL(destination);
-
-        // Only lookup hostname if we have a non empty string for it
-        var passHostname = destURL.hostname;
-        if(passHostname == "") {
-            if( (match = destination.match(/www(\.[a-zA-Z]+)+/)) != null){
-                passHostname = match[0];
-            }
-        }
-
-
-        if(passHostname != "") {
-            // use DNS to translate hostname to IP
-            dns.lookup(destURL.hostname, (error, addr, fam) => {
-                
-                if(error){
-                    console.log("Error parsing this hostname: " + destURL.hostname);
-                    return;
-                }
-                
-                console.log("Parsed URL translated to IP"  + fam + ": " + addr);
-
-                // create a TCP connection to the webserver's IP (using 80 for any HTTP requests)
-                ProxyCLient = net.connect(80 ,addr, () => {
-                    console.log("connected to webserver");
-                })
-                ProxyCLient.write(data);
-
-                ProxyCLient.on('data', (dataFromWebServer) => {
-                    socket.write(dataFromWebServer)
-                })
-            });
-        }
-
-        // Log the parsed user name (may be empty if no valid one was found)
-        console.log("Destination Hostname: " + passHostname); */
-
         
     });
 });
 
 proxyServer.listen(3000, "localhost");
-
